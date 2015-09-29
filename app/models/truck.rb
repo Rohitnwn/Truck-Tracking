dbc19db139475bcb03b6a69a8a255614a77d63b0
Rohit Nawani <rohitnwn@gmail.com>
2015-09-30 00:28:48 +0530

    Updated readme doc

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,12 +1,12 @@
 class Truck < ActiveRecord::Base
-	def self.near(lat, lng)
-		if lat.present? and lng.present?
-			# finding nearest five in 5km radius of source
-			Truck.find_by_sql("SELECT * FROM (SELECT id, applicant, facility_type, status, food_items, days_hours, latitude, longitude, st_distance_sphere(st_point(#{lng}, #{lat}), st_point(trucks.longitude, trucks.latitude) ) distance FROM trucks ORDER BY distance LIMIT 5) as near_trucks WHERE distance <= 5000")
-		else
-			[]	
-		end	
-	end
-
+  def self.near(lat, lng)
+    if lat.present? and lng.present?
+      # finding nearest five in of source
+      trucks = Truck.find_by_sql("SELECT id, applicant, facility_type, status, food_items, days_hours, latitude, longitude, st_distance_sphere(st_point(#{lng}, #{lat}), st_point(trucks.longitude, trucks.latitude) ) distance FROM trucks ORDER BY distance LIMIT 5")
+      #Taking trucks only inside 5km radius
+      trucks.select { |tr| tr.distance <= 5000 }
+    else
+      []	
+    end	
+  end
 end
-
